c6fbd12b8c6b72413430ba305c373bdea80580fb
AtefehGhanizadeh <94842580+AtefehGhanizadeh@users.noreply.github.com>
2022-07-31 00:28:02 +0430

    Add files via upload

---
+++ products.js
@@ -0,0 +1,43 @@
+export const productsData=[
+
+    {
+        id:1,
+        title:"product1",
+        price:1,
+        image:"images/image1.jpg"
+    },
+
+    {
+        id:2,
+        title:"product2",
+        price:2,
+        image:"images/image2.jpg"
+    },
+
+    {
+        id:3,
+        title:"product3",
+        price:3,
+        image:"images/image3.jpg"
+    },
+    {
+        id:4,
+        title:"product4",
+        price:4,
+        image:"images/image4.jpg"
+    },
+
+    {
+        id:5,
+        title:"product5",
+        price:5,
+        image:"images/image5.jpg"
+    },
+    {
+        id:6,
+        title:"product6",
+        price:6,
+        image:"images/image6.jpg"
+    }
+
+]
